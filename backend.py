62e3d36946d0060bbeb54f02a6a7cb93e8f9ca6d
Colby J <seejaytd@gmail.com>
2025-08-13 11:26:27 -0400

    corrected backend.py

--- backend.py
+++ backend.py
@@ -2,16 +2,16 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
 # Define some variables.
-ip = '0.0.0.0'
+ip = "0.0.0.0"
 port = 8000
 server_address = (ip, port)
 
 # Define and init a variable called "api" using the classes we imported. 
 api = HTTPServer(
-    server_address=server_address,
-    RequestHandlerClass=SimpleHTTPRequestHandler
+    server_address=server_address, RequestHandlerClass=SimpleHTTPRequestHandler
 )
 
 # Start the HTTP server.
 print("Listening for HTTP requests...")
+
 api.serve_forever()
